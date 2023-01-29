1ff92e52bd125cedac64444946b5d73ec10e70e3
JNeed <joshua.needleman510@gmail.com>
2023-01-29 11:17:31 -0500

    data not showing in bokeh plot. gitignore *.html

--- viz.py
+++ viz.py
@@ -5,16 +5,17 @@ import pandas as pd
 
 table = main_web()
 
-print(table)
+# print(table)
 # # # prepare some data
+x = table['Date']
 # x = pd.to_datetime(table['Date'], unit='d')
-# y = table['PTS']
 
+y = table['PTS']
 # # # create a new plot with a title and axis labels
-# p = figure(title="Donovan Mitchell Points", x_axis_label="Date", y_axis_label="Points Scored")
+p = figure(title="Donovan Mitchell Points", x_axis_label="Date", y_axis_label="Points Scored")
 
 # # # add a line renderer with legend and line thickness
-# p.line(x, y, legend_label="Temp.", line_width=2)
+p.line(x, y, legend_label="Points", line_width=2)
 
 # # # show the results
-# show(p)
+show(p)

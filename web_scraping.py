191b415920407fa95967fad56ed83bd30618b6cb
JNeed <joshua.needleman510@gmail.com>
2023-01-29 10:30:49 -0500

    factored out playwright startup into one fxn

--- web_scraping.py
+++ web_scraping.py
@@ -7,42 +7,41 @@ def name_handling(player_name):
     fname,lname = player_name.lower().split()
     lname_short = lname
     fletter_lname = lname[0]
-
     if len(lname) > 4:
         lname_short = lname_short[:5]
     fname_short = fname
     if len(fname)> 1:
         fname_short = fname_short[:2]
-
     return (fname_short,lname_short)
 
-def player_gamelog(player_name, year, p):
-    chromium = p.chromium
-    browser = chromium.launch(headless=True)
-    page = browser.new_page()
-    html_table = ''
-
-    fname_short,lname_short = name_handling(player_name)
-
-    base = "https://www.basketball-reference.com/players/"
-
-    full_url = base + lname_short[0] + '/' + lname_short + fname_short + '01/gamelog/'+ str(year)
-    try:
-        page.goto(full_url,timeout=1500)
-    except:
-        html_table = pd.read_html(full_url)[7]
-
+def table_process_write(fname_short,lname_short,html_table):
     header_bool = html_table['Rk'].apply(lambda x: x!= 'Rk')
     html_table = html_table[header_bool]
     html_table = html_table.replace(['Inactive', 'Did Not Dress', 'Did Not Play'],-1)
     xl = fname_short +'_'+ lname_short + '.xlsx'
     html_table.to_excel(xl)
+
+def playwright_start(p):
+    chromium = p.chromium
+    browser = chromium.launch(headless=True)
+    page = browser.new_page()
+    return (page,browser)
+
+def player_gamelog(player_name, year, p):
+    page,browser = playwright_start(p)
+    fname_short,lname_short = name_handling(player_name)
+
+    base = "https://www.basketball-reference.com/players/"
+    full_url = base + lname_short[0] + '/' + lname_short + fname_short + '01/gamelog/'+ str(year)
+
+    try:
+        page.goto(full_url,timeout=1500)
+    except:
+        html_table = pd.read_html(full_url)[7]
+        table_process_write(fname_short, lname_short, html_table)
+
     page.close()
     browser.close()
-
-
-
-
 
 with sync_playwright() as p:
     player = 'Donovan Mitchell'

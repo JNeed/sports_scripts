96910420583a450d53a1e5c72db05497130ec2ba
JNeed <joshua.needleman510@gmail.com>
2023-01-29 10:21:26 -0500

    factored out name handling into name handling fxn

--- web_scraping.py
+++ web_scraping.py
@@ -2,12 +2,8 @@ from playwright.sync_api import sync_playwright
 import pandas as pd
 from io import StringIO
 
-def player_gamelog(player_name, year, p):
-    chromium = p.chromium
-    browser = chromium.launch(headless=True)
-    page = browser.new_page()
-    html_table = ''
 
+def name_handling(player_name):
     fname,lname = player_name.lower().split()
     lname_short = lname
     fletter_lname = lname[0]
@@ -18,9 +14,19 @@ def player_gamelog(player_name, year, p):
     if len(fname)> 1:
         fname_short = fname_short[:2]
 
+    return (fname_short,lname_short)
+
+def player_gamelog(player_name, year, p):
+    chromium = p.chromium
+    browser = chromium.launch(headless=True)
+    page = browser.new_page()
+    html_table = ''
+
+    fname_short,lname_short = name_handling(player_name)
+
     base = "https://www.basketball-reference.com/players/"
 
-    full_url = base + fletter_lname + '/' + lname_short + fname_short + '01/gamelog/'+ str(year)
+    full_url = base + lname_short[0] + '/' + lname_short + fname_short + '01/gamelog/'+ str(year)
     try:
         page.goto(full_url,timeout=1500)
     except:
@@ -29,12 +35,10 @@ def player_gamelog(player_name, year, p):
     header_bool = html_table['Rk'].apply(lambda x: x!= 'Rk')
     html_table = html_table[header_bool]
     html_table = html_table.replace(['Inactive', 'Did Not Dress', 'Did Not Play'],-1)
-    xl = fname +'_'+ lname + '.xlsx'
+    xl = fname_short +'_'+ lname_short + '.xlsx'
     html_table.to_excel(xl)
     page.close()
     browser.close()
-
-# idea: replace inactive, did not dress, and did not play with -1
 
 
 

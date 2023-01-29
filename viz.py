bb39b47873148fb731565ab63f2df1bc245233d1
JNeed <joshua.needleman510@gmail.com>
2023-01-29 13:34:06 -0500

    adjusted quantiles

--- viz.py
+++ viz.py
@@ -1,7 +1,7 @@
 from bokeh.plotting import figure, show
 from web_scraping import main_web
 import pandas as pd
-from bokeh.models import BoxAnnotation
+from bokeh.models import BoxAnnotation,DatetimeTickFormatter
 
 player = 'Donovan Mitchell'
 year = 2023 # must be in format yyyy
@@ -16,13 +16,14 @@ played_bool = y.apply(lambda x: x != -1)
 p = figure(title=player+ " " + str(year) + " Season Points", x_axis_label="Date", y_axis_label="Points Scored", x_axis_type='datetime')
 
 
-low_box = BoxAnnotation(top=.2 * y[played_bool].max(), fill_alpha=0.2, fill_color='red')
-high_box = BoxAnnotation(bottom=.8 * y[played_bool].max(), fill_alpha=0.2, fill_color='green')
+low_box = BoxAnnotation(top=y[played_bool].quantile(.2), fill_alpha=0.2, fill_color='red')
+high_box = BoxAnnotation(bottom=y[played_bool].quantile(.8), fill_alpha=0.2, fill_color='green')
 
 p.add_layout(low_box)
 p.add_layout(high_box)
 
 p.scatter(x[played_bool], y[played_bool], legend_label="Points", color = 'blue',size=5)
 p.scatter(x[~played_bool], y[~played_bool], legend_label="Didn't Play", color = 'red',marker='x',size = 10)
+p.xaxis[0].formatter = DatetimeTickFormatter(months="%B-%y")
 
 show(p)

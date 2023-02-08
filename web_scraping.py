331b787fa844db35cca3f15d219c9e67896d950e
JNeed <joshua.needleman510@gmail.com>
2023-02-08 07:51:30 -0500

    now can handle similar names and 3 word names

--- web_scraping.py
+++ web_scraping.py
@@ -2,8 +2,40 @@ from playwright.sync_api import sync_playwright
 import pandas as pd
 from io import StringIO
 
+EXCEPTIONS = {
+'Javonte Green':'https://www.basketball-reference.com/players/g/greenja02/gamelog/2023',
+'Jalen Green':'https://www.basketball-reference.com/players/g/greenja05/gamelog/2023',
+'JaMychal Green':'https://www.basketball-reference.com/players/g/greenja01/gamelog/2023',
+'Bogdan Bogdanovic':'https://www.basketball-reference.com/players/b/bogdabo01/gamelog/2023',
+'Bojan Bogdanovic':'https://www.basketball-reference.com/players/b/bogdabo02/gamelog/2023',
+'Jalen McDaniels':'https://www.basketball-reference.com/players/m/mcdanja01/gamelog/2023',
+'Jaden McDaniels':'https://www.basketball-reference.com/players/m/mcdanja02/gamelog/2023',
+'Julian Champagnie':'https://www.basketball-reference.com/players/c/champju02/gamelog/2023',
+'Justin Champagnie':'https://www.basketball-reference.com/players/c/champju01/gamelog/2023',
+'Ziaire Williams':'https://www.basketball-reference.com/players/w/willizi02/gamelog/2023',
+'Zion Williamson':'https://www.basketball-reference.com/players/w/willizi01/gamelog/2023',
+'Markieff Morris':'https://www.basketball-reference.com/players/m/morrima02/gamelog/2023',
+# 'Marcus Morris':'https://www.basketball-reference.com/players/m/morrima03/gamelog/2023',
+'Marcus Morris Sr.':'https://www.basketball-reference.com/players/m/morrima03/gamelog/2023',
+'James Johnson':'https://www.basketball-reference.com/players/j/johnsja01/gamelog/2023',
+'Jalen Johnson':'https://www.basketball-reference.com/players/j/johnsja05/gamelog/2023',
+'Jalen Smith':'https://www.basketball-reference.com/players/s/smithja04/gamelog/2023',
+# 'Jabari Smith':'https://www.basketball-reference.com/players/s/smithja05/gamelog/2023',
+'Jabari Smith Jr.':'https://www.basketball-reference.com/players/s/smithja05/gamelog/2023',
+'Jaylin Williams':'https://www.basketball-reference.com/players/w/willija07/gamelog/2023',
+'Jalen Williams':'https://www.basketball-reference.com/players/w/willija06/gamelog/2023',
+'Keon Johnson':'https://www.basketball-reference.com/players/j/johnske07/gamelog/2023',
+'Keldon Johnson':'https://www.basketball-reference.com/players/j/johnske04/gamelog/2023'
+}
+
+# I think still having problems if name has 3 words in it because just get first name last name
+# EXCEPTIONS =  {k.lower(): v for k, v in EXCEPTIONS.items()}
+
+
 def name_handling(player_name):
-    fname,lname = player_name.lower().split()
+    name_ls = player_name.lower().split()
+    fname,lname = name_ls[0],name_ls[1]
+    # fname,lname = player_name.split()
     lname_short = lname
     fletter_lname = lname[0]
     if len(lname) > 4:
@@ -35,6 +67,17 @@ def player_gamelog(player_name, year, p):
     base = "https://www.basketball-reference.com/players/"
     full_url = base + lname_short[0] + '/' + lname_short + fname_short + '01/gamelog/'+ str(year)
 
+    # player_name_ls = player_name.split()
+    # search_term = player_name_ls[0].lower() + " " + player_name_ls[1].lower()
+    if player_name in EXCEPTIONS.keys():
+        try:
+            page.goto(EXCEPTIONS[player_name],timeout=1500)
+        except:
+            html_table = pd.read_html(EXCEPTIONS[player_name])[7]
+            table = table_process_write(fname_short, lname_short, False, html_table)
+            page.close()
+            browser.close()
+            return table
     try:
         page.goto(full_url,timeout=1500)
     except:
@@ -54,27 +97,27 @@ def main_web(player, year):
 
 
 # exceptions list
-# 'Javonte Green':'https://www.basketball-reference.com/players/g/greenja02.html'
-# 'Jalen Green':'https://www.basketball-reference.com/players/g/greenja05.html'
-# 'JaMychal Green':'https://www.basketball-reference.com/players/g/greenja01.html'
-# 'Bogdan Bogdanovic':'https://www.basketball-reference.com/players/b/bogdabo01.html'
-# 'Bojan Bogdanovic':'https://www.basketball-reference.com/players/b/bogdabo02.html'
-# 'Jalen McDaniels':'https://www.basketball-reference.com/players/m/mcdanja01.html'
-# 'Jaden McDaniels':'https://www.basketball-reference.com/players/m/mcdanja02.html'
-# 'Julian Champagnie':'https://www.basketball-reference.com/players/c/champju02.html'
-# 'Justin Champagnie':'https://www.basketball-reference.com/players/c/champju01.html'
-# 'Ziaire Williams':'https://www.basketball-reference.com/players/w/willizi02.html'
-# 'Zion Williamson':'https://www.basketball-reference.com/players/w/willizi01.html'
-# 'Markieff Morris':'https://www.basketball-reference.com/players/m/morrima02.html'
-# 'Marcus Morris Sr'.:'https://www.basketball-reference.com/players/m/morrima03.html'
-# 'James Johnson':'https://www.basketball-reference.com/players/j/johnsja01.html'
-# 'Jalen Johnson':'https://www.basketball-reference.com/players/j/johnsja05.html'
-# 'Jalen Smith':'https://www.basketball-reference.com/players/s/smithja04.html'
-# 'Jabari Smith Jr.':'https://www.basketball-reference.com/players/s/smithja05.html'
-# 'Jaylin Williams':'https://www.basketball-reference.com/players/w/willija07.html'
-# 'Jalen Williams':'https://www.basketball-reference.com/players/w/willija06.html'
-# 'Keon Johnson':'https://www.basketball-reference.com/players/j/johnske07.html'
-# 'Keldon Johnson':'https://www.basketball-reference.com/players/j/johnske04.html'
+# 'Javonte Green':'https://www.basketball-reference.com/players/g/greenja02/gamelog/2023'
+# 'Jalen Green':'https://www.basketball-reference.com/players/g/greenja05/gamelog/2023'
+# 'JaMychal Green':'https://www.basketball-reference.com/players/g/greenja01/gamelog/2023'
+# 'Bogdan Bogdanovic':'https://www.basketball-reference.com/players/b/bogdabo01/gamelog/2023'
+# 'Bojan Bogdanovic':'https://www.basketball-reference.com/players/b/bogdabo02/gamelog/2023'
+# 'Jalen McDaniels':'https://www.basketball-reference.com/players/m/mcdanja01/gamelog/2023'
+# 'Jaden McDaniels':'https://www.basketball-reference.com/players/m/mcdanja02/gamelog/2023'
+# 'Julian Champagnie':'https://www.basketball-reference.com/players/c/champju02/gamelog/2023'
+# 'Justin Champagnie':'https://www.basketball-reference.com/players/c/champju01/gamelog/2023'
+# 'Ziaire Williams':'https://www.basketball-reference.com/players/w/willizi02/gamelog/2023'
+# 'Zion Williamson':'https://www.basketball-reference.com/players/w/willizi01/gamelog/2023'
+# 'Markieff Morris':'https://www.basketball-reference.com/players/m/morrima02/gamelog/2023'
+# 'Marcus Morris Sr'.:'https://www.basketball-reference.com/players/m/morrima03/gamelog/2023'
+# 'James Johnson':'https://www.basketball-reference.com/players/j/johnsja01/gamelog/2023'
+# 'Jalen Johnson':'https://www.basketball-reference.com/players/j/johnsja05/gamelog/2023'
+# 'Jalen Smith':'https://www.basketball-reference.com/players/s/smithja04/gamelog/2023'
+# 'Jabari Smith Jr.':'https://www.basketball-reference.com/players/s/smithja05/gamelog/2023'
+# 'Jaylin Williams':'https://www.basketball-reference.com/players/w/willija07/gamelog/2023'
+# 'Jalen Williams':'https://www.basketball-reference.com/players/w/willija06/gamelog/2023'
+# 'Keon Johnson':'https://www.basketball-reference.com/players/j/johnske07/gamelog/2023'
+# 'Keldon Johnson':'https://www.basketball-reference.com/players/j/johnske04/gamelog/2023'
 
 
 

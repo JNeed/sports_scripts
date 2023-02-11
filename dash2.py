564aee857bd5b8e7e0de8ff2905372de38e80ae5
JNeed <joshua.needleman510@gmail.com>
2023-02-10 22:02:47 -0500

    made "All" the team default

--- dash2.py
+++ dash2.py
@@ -4,17 +4,21 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 from web_scraping import main_web
+import plotly.graph_objects as go
 
 df = get_table('player', 'sqlite:///db/nba.db')
-teams = np.sort(df.TEAM.unique())
+teams = np.append(np.sort(df.TEAM.unique()),"All")
 bos = df.query("TEAM == 'Bos'")
 
 app = Dash(__name__)
+# print(df.NAME)
 
 app.layout = html.Div([
-    dcc.Dropdown(teams, value = 'Bos', id='teams'),
+    # dcc.Dropdown(teams, value = 'Bos', id='teams'),
+    dcc.Dropdown(teams, value = 'All', id='teams'),
     # html.Div(id='dd-output-container'),
-    dcc.Dropdown(options=bos.NAME, value = 'Jayson Tatum', id='players'),
+    # dcc.Dropdown(options=bos.NAME,value='Jayson Tatum', id='players'),
+    dcc.Dropdown(options=df.NAME.tolist(), value='Jayson Tatum', id='players'),
     dcc.Dropdown(options=['FG', 'FGA', 'FG%', '3P', '3PA', '3P%', 'FT', 'FTA', 'FT%', 'ORB',
        'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS', 'GmSc', '+/-'],id='stats'),
     dcc.Graph(id = 'graph')
@@ -26,6 +30,8 @@ app.layout = html.Div([
     Input('teams', 'value')
 )
 def update_output(value):
+    if value == "All":
+        return df.NAME
     players = df.query('TEAM == @value').NAME
     return players
 
@@ -37,6 +43,9 @@ def update_output(value):
 
 def update_graph(p,stat):
     player = main_web(p, 2023)
+    player["Minutes Played"] = player.MP.str.replace(":",".").astype(float)
+    m = player["Minutes Played"].min()
+    player["Minutes Played"] = player["Minutes Played"].fillna(m)
     player.PTS = player['PTS'].astype('int32')
     player.FG = player['FG'].astype('int32')
     player.FGA = player['FGA'].astype('int32')
@@ -58,8 +67,11 @@ def update_graph(p,stat):
     player.GmSc = player['GmSc'].astype('float')
     player['+/-'] = player['+/-'].astype('int32')
     injured = pd.cut(player.PTS,[-1,0,1000],right=False,labels=["Didn't play", "Played"])
-
-    fig = px.scatter(player, 'Date',stat,color = injured)
+    fig = px.scatter(player, 'Date',stat,symbol = injured,color=player["Minutes Played"],color_continuous_scale='blues')
+    fig.update_layout(legend=dict(
+    yanchor="bottom",
+    xanchor="left"),legend_title_text='Played Status',plot_bgcolor='#dbdbdb')
+    # idea: change background color of plot to darker color so you can see lighter points
     return fig
 
 

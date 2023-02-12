e9ee0f49f2a863afe02a9379aadb6f3290fdfe36
JNeed <joshua.needleman510@gmail.com>
2023-02-12 16:39:31 -0500

    fixed injured symbols; legend still needs update

--- multi.py
+++ multi.py
@@ -58,27 +58,13 @@ def update_player(player_names):
     return player_df_json_ls
     
 
-
-    # player = main_web(val, 2023)
-    # player["Minutes Played"] = player.MP.str.replace(":",".").astype(float)
-    # m = player["Minutes Played"].min()
-    # player["Minutes Played"] = player["Minutes Played"].fillna(m)
-    # for col in player.columns[10:]:
-    #     if ('%' in col) or (col == 'GmSc'):
-    #         player[col] = player[col].astype('float')
-    #     elif ('%' not in col) and (col != 'GmSc') and (col != 'Minutes Played'):
-    #         player[col] = player[col].astype('int32')
-            
-    # injured = player.PTS.apply(lambda x: "Didn't Play" if x == -1 else "Played")
-    # player["Played Status"] = injured
-    # return player.to_json(date_format='iso', orient='split')
-
 @app.callback(
     Output('graph','figure'),
     Input('stats','value'),
     Input('intermediate-value','data')
 )
 def update_graph(stat, players_json_dfs):
+    #need another a Dcc name storer and need a callback to actually store names in there; might store names[-1]
     # return go.Figure()
     color_scales = ['Blues','BuGn','Greys','Oranges',"Purples"]
     if players_json_dfs == None or stat == None:
@@ -91,19 +77,14 @@ def update_graph(stat, players_json_dfs):
         if i == 0:
             fig = px.scatter(player, 'Date',stat,symbol = player["Played Status"],color=player["Minutes Played"],color_continuous_scale=color_scales[i],symbol_sequence=symbols)
         else:
-            # fig.add_trace(px.scatter(player, 'Date',stat,color = player['Minutes Played'],color_continuous_scale='redor',symbol_sequence=symbols))
+            injured_symbol = player["Played Status"].apply(lambda x: 'circle' if x=="Played" else injured_symbols[i])
+            fig.add_scatter(x = player.Date, y=player[stat],mode='markers',marker_color = player['Minutes Played'],marker=dict(size=6,colorscale = color_scales[i], symbol=injured_symbol))
 
-            fig.add_trace(go.Scatter(x=player.Date,y=player[stat],mode='markers',marker_color = player['Minutes Played'],marker=dict(size=8,colorscale = color_scales[i])))
+            # fig.add_trace(go.Scatter(x=player.Date,y=player[stat],mode='markers',marker_color = player['Minutes Played'],marker=dict(size=6,colorscale = color_scales[i])))
 
-            # fig.add_trace(go.Scatter(x=player.Date,y=player[stat],mode='markers'),marker_color = player['Minutes Played'],colorscale='redor')
-    fig.update_layout(legend=dict(
+            fig.update_layout(legend=dict(
             yanchor="bottom",
             xanchor="left"),legend_title_text='Played Status',plot_bgcolor='#dbdbdb')
     return fig
-    # fig = px.scatter(player, 'Date',stat,symbol = player["Played Status"],color=player["Minutes Played"],color_continuous_scale='blues',symbol_sequence=symbols)
-    # fig.update_layout(legend=dict(
-    #     yanchor="bottom",
-    #     xanchor="left"),legend_title_text='Played Status',plot_bgcolor='#dbdbdb')
-    # return fig
-    
+
 app.run_server(debug=True)
